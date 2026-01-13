e2808537e8e603df20b301e28949a9fc162f5670
andyhuang0726 <101930994+andyhuang0726@users.noreply.github.com>
2026-01-13 22:37:12 +0800

    Create dml.sql

---
+++ DB/dml.sql
@@ -0,0 +1,7 @@
+INSERT INTO SeatingChart(FLOOR_NO, SEAT_NO) 
+VALUES (1,1),(1,2),(1,3),(2,1),(2,2),(2,3);
+
+INSERT INTO Employee(EMP_ID, NAME, EMAIL) 
+VALUES ('10001','Andy','andy@test.com'),
+        ('10002','Ben','ben@test.com'),
+        ('10003','Cindy','cindy@test.com');
