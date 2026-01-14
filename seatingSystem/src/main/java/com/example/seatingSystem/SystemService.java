c96af07f8780050ea09b7811dc4fe34e4e320b2d
andyhuang0726 <101930994+andyhuang0726@users.noreply.github.com>
2026-01-14 20:50:07 +0800

    Update SystemService.java

--- seatingSystem/src/main/java/com/example/seatingSystem/SystemService.java
+++ seatingSystem/src/main/java/com/example/seatingSystem/SystemService.java
@@ -1,8 +1,15 @@
 package com.example.seatingSystem;
 
 import org.springframework.stereotype.Service;
+import com.example.seatingSystem.SystemRepository;
 
 @Service
 public class SystemService {
+    private final SystemRepository repository;
 
+    public SystemService(SystemRepository repository) {
+        this.repository = repository;
+    }
 }
+
+
