4135e997050d0f2b1930ca5ded675db0f1924921
andyhuang0726 <101930994+andyhuang0726@users.noreply.github.com>
2026-01-15 16:28:49 +0800

    Update SystemController.java

--- seatingSystem/src/main/java/com/example/seatingSystem/SystemController.java
+++ seatingSystem/src/main/java/com/example/seatingSystem/SystemController.java
@@ -1,7 +1,6 @@
 package com.example.seatingSystem;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import com.example.seatingSystem.SystemService;
 
 @RestController
@@ -13,4 +12,10 @@ public class SystemController {
         this.service = service;
     }
 
+    public List<Map<String, Object>> getAllSeats() {
+        // 直接呼叫 Service 層
+        return service.getAllSeats();
+    }
+
 }
+
